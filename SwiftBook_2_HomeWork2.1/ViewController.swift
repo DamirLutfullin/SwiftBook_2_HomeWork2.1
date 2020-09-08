ab99faf868359e233a13ae0df75cff3148af6273
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-09-08 22:33:50 +0500

    Initial Commit

---
+++ SwiftBook_2_HomeWork2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBook_2_HomeWork2.1
+//
+//  Created by Damir Lutfullin on 08.09.2020.
+//  Copyright © 2020 DamirLutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
